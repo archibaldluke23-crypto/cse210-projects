9b3122c10a8e43c8b445e93f4757064bc651f34f
Luke Archibald <archibaldluke23@gmail.com>
2026-01-13 17:27:53 -0700

    wrote some notes

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,41 @@
 using System;
+// This is a one line comment 
 
-class Program
+/*
+This is a 
+multi-line comment
+*/
+namespace Sandbox
 {
-    static void Main(string[] args)
+    public class Program
     {
-        Console.WriteLine("Hello Sandbox World!");
+        public static void Main()
+        {
+            Console.WriteLine("Hello World!");
+
+            /* 
+            WriteLine will take up the entire line in the terminal.
+            But just Write will take up a portion of the line
+            */
+            Console.Write("Hello "); Console.WriteLine("World!!!");
+            
+
+            // This will get input from the user
+            Console.Write("What is your favorite color? ");
+            string color = Console.ReadLine();
+
+            int x = 5;
+            Console.WriteLine("Value of x is " + x);
+
+            int y = 3;
+            if (x > y) // Put the condision in parentheses without a colon
+            {
+                Console.WriteLine("greater");
+            }
+
+            string school = "BYU-Idaho";
+            Console.WriteLine($"I am studying at {school}."); // Use $ as you would use f in python
+        }
     }
 }
+
