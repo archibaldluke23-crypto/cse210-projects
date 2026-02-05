1ddffb322a7ad8f1ccaecd4cb08372242a651cc9
Luke Archibald <archibaldluke23@gmail.com>
2026-02-05 15:35:24 -0700

    Did the example during class

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -8,12 +8,7 @@
 //     {
 //         public static void Main()
 //         {
-//             //  StreamReader linesRead = new StreamReader("C:\\Documents\\CSE 210 (programming with classes)\\cse210-projects\\prove\\Develop02\\Entries.txt");
-//             // string line = linesRead.ReadLine();
-//             // Console.WriteLine(line);
-
-//             Random num = new Random();
-//         Console.WriteLine(num);
+           
 //         }
 //     }
 // }
