98e893454f09594c77203001bcd83b37c2ae76b7
Luke Archibald <archibaldluke23@gmail.com>
2026-02-24 12:17:45 -0700

    Completed learning04

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,9 +1,20 @@
 using System;
+using System.Reflection.Metadata;
 
 class Program
-{
+{ // Mary Waters - European History
+//The Causes of World War II by Mary Waters
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment1 = new Assignment("Joe", "mamma");
+        Console.WriteLine(assignment1.GetSummary());
+
+        MathAssignment assignment2 = new MathAssignment("Mark", "Word Problems", "2.3", "5-15");
+        Console.WriteLine(assignment2.GetSummary());
+        Console.WriteLine(assignment2.GetHomeworkList());
+
+        WritingAssignment assignment = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(assignment.GetSummary());
+        Console.WriteLine(assignment.GetWritingInformation());
     }
 }
