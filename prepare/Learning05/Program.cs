76b109c7811d78e4b13fdd4b094df331f5bc27ba
Luke Archibald <archibaldluke23@gmail.com>
2026-03-09 18:21:30 -0600

    finished learning activity 5

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        
+        Square square1 = new Square("blue", 5);
+        Rectangle rectangle1 = new Rectangle("yellow", 5, 4);
+        Circle circle1 = new Circle("green", 5);
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(square1);
+        shapes.Add(rectangle1);
+        shapes.Add(circle1);
+        
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine(shape.GetColor());
+            Console.WriteLine(shape.GetArea());
+        }
+        
+
     }
 }
